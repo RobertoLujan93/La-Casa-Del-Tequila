1a5b7747ad640215980946b62cc44c831c019a5c
Roberto Lujan <robertolujan93@gmail.com>
2022-04-16 17:55:14 -0500

    Agregar Navbar y Productos

---
+++ src/Components/Productos.js
@@ -0,0 +1,26 @@
+import Card from "./Card"
+
+function Productos() {
+    return (
+    <div className= "w-full mx-auto flex flex-wrap justify-center gap-10">
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+    </div>
+  )
+}
+
+export default Productos
