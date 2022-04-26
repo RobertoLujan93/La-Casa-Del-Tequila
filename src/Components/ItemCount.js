208b11f73c663a3fe05368da915debfbb85d298e
Roberto Lujan <robertolujan93@gmail.com>
2022-04-26 17:20:27 -0500

    Actualizar contador

--- src/Components/ItemCount.js
+++ src/Components/ItemCount.js
@@ -1,29 +1,28 @@
-import { useState } from "react"
+import { useState } from "react";
 
-const ItemCount = ({stock, initial}) => {
-  const [count, setCount] = useState(Number(initial))
-	const resHandler = () => {
-    if(count > initial){
-      setCount(count - 1)
+const ItemCount = ({ stock, initial, onAdd }) => {
+  const [count, setCount] = useState(initial);
+  const resHandler = () => {
+    if (count > initial) {
+      setCount(count - 1);
     }
-  }
+  };
   const addHandler = () => {
-    if (count < stock){
-      setCount(count + 1)
+    if (count < stock) {
+      setCount(count + 1);
     }
-  }
+  };
 
-	return (
-		<div>
-		<div className="flex justify-evenly mt-2 bg-gray-200 rounded-xl p-4">
-			<button onClick={resHandler}>-</button>
-			<span>{count}</span>
-			<button onClick={addHandler}>+</button>
-		</div>
-			<button className="btn btn-primary mx-32 mt-4">Comprar</button>
-		</div>
-	)
-}
+  return (
+    <div>
+      <div className="flex justify-evenly mt-2 bg-gray-200 rounded-xl p-4">
+        <button onClick={resHandler}>-</button>
+        <span>{count}</span>
+        <button onClick={addHandler}>+</button>
+      </div>
+      <button onClick = {() => onAdd(count)}  className="btn btn-primary mx-32 mt-4">Agregar al Carrito</button>
+    </div>
+  );
+};
 
-
-export default ItemCount
+export default ItemCount;
