a2b39e01ef91af4d6ecc697347f1f119367391cc
Roberto Lujan <robertolujan93@gmail.com>
2022-05-26 19:57:58 -0500

    Estilos

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -3,14 +3,23 @@ import { Link } from "react-router-dom";
 const Item = (props) => {
   const {id,title,price,pictureUrl} = props.item
   return (
-  <div className="card card-compact w-96 bg-base-100 shadow-xl">
-    <figure><img src={pictureUrl} alt="Imagen de Tequila" /></figure>
-    <div className="card-body">
-      <h2 className="text-center text-xl">{title}</h2>
-      <h4 className="text-center font-bold text-xl">${price}</h4>
-      <Link to= {`/tequila/${id}`} className="btn mx-32 mt-4 text-white bg-black hover:text-black hover:bg-white">Ver Detalle</Link>
+<div className="max-w-sm bg-white rounded-lg ml-6 mr-6 card">
+        <img className="p-8" src={pictureUrl} alt="product image" />
+    <div className="px-5 pb-5">
+            <h5 className="text-lg font-semibold tracking-tight text-black">{title}</h5>
+        <div className="flex items-center mt-5 mb-3">
+            <svg className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+            <svg className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+            <svg className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+            <svg className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+            <svg className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+            <span className="bg-black text-white text-xs font-semibold mr-2 px-2.5 py-0.5 rounded ml-3">5.0</span>
+        </div>
+        <div className="flex justify-between items-center">
+            <span className="text-xl font-bold text-black">${price}</span>
+            <Link to= {`/tequila/${id}`} className="btn px-3 py-2 text-white bg-black hover:text-black hover:bg-white text-xs font-bold rounded">Ver Detalle</Link>
+        </div>
     </div>
-  </div>
+</div>
   )}
 export default Item;
-
