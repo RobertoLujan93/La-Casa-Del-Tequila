2a3c6e1ad6d4dd42f84d344cf4146ef28ec1000f
Roberto Luján <Roberto@Silvias-MacBook-Air-5.local>
2022-05-31 19:11:34 -0500

    Estilos banner

--- src/Components/ItemListContainer.js
+++ src/Components/ItemListContainer.js
@@ -3,6 +3,17 @@ import ItemList from "./ItemList"
 function ItemListContainer(props) {
     return ( 
     <>
+    <div className="banner">
+      <img 
+        src={process.env.PUBLIC_URL+"./imagenes/banner.jpg"} alt="imagen banner">
+      </img>
+      <h1>
+        LA CASA DEL TEQUILA
+      </h1>
+      <h3>
+        Las mejores historias comienzan con una botella de tequila
+      </h3>
+    </div>
     <ItemList/>
     </>
   )
--- src/Components/context/CartContext.js
+++ src/Components/context/CartContext.js
@@ -1,11 +1,12 @@
-import React, {createContext, useContext, useState} from "react";
+import React, {createContext, useContext} from "react";
+import { useLocalStorage } from "../../useLocalStorage";
 
 const CartContext = createContext()
 export const useCartContext = () => useContext(CartContext)
 
 const CartContextProvider = ({children}) => {
 
-	const [cart,setCart] = useState([])
+	const [cart, setCart] =  useLocalStorage('carrito', [])
 
 	const isInCart = (id) => cart.find (producto => producto.id ===id)
 
